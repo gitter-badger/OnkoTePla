1e46780c07399d341b9704e5716ee4d4584113e6
xelor <kingxelor@gmail.com>
2015-04-16 17:51:36 +0200

    Added NotifyerExtension

---
+++ xIT.Lib/Notifyer/PropertyChangedNotifierExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace xIT.Lib.Notifyer
+{
+	public static class PropertyChangedNotifierExtension
+	{
+		public static void ChangeAndNotify<T> (this PropertyChangedEventHandler handler, object sender,
+								   ref T field, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value)) return;
+
+			field = value;
+
+			var tmpHandler = handler;
+			if (tmpHandler != null)
+			{
+				tmpHandler(sender, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
