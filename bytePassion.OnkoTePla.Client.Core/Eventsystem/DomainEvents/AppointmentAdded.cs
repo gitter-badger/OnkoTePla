fc34b289d4b2bc1c75bca119d03c8220d6986d3a
xelor <kingxelor@gmail.com>
2015-05-19 12:02:21 +0200

    DomainEvent "AppointmentAdded" extended

--- bytePassion.OnkoTePla.Client.Core/Eventsystem/DomainEvents/AppointmentAdded.cs
+++ bytePassion.OnkoTePla.Client.Core/Eventsystem/DomainEvents/AppointmentAdded.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using bytePassion.OnkoTePla.Client.Core.Eventsystem.Eventbase;
+using bytePassion.OnkoTePla.Contracts.Appointments;
 
 
 namespace bytePassion.OnkoTePla.Client.Core.Eventsystem.DomainEvents
 {
 	public class AppointmentAdded : DomainEvent
 	{
-		public AppointmentAdded(Guid aggregateID, int versionOfAggregate, Guid commandID) 
-			: base(aggregateID, versionOfAggregate, commandID) {}
+		private readonly Appointment addedAppointment;
+
+		public AppointmentAdded(Guid aggregateID, int versionOfAggregate, Guid commandID, Appointment addedAppointment)
+			: base(aggregateID, versionOfAggregate, commandID)
+		{
+			this.addedAppointment = addedAppointment;
+		}
+
+		public Appointment AddedAppointment
+		{
+			get { return addedAppointment; }
+		}
 	}
 }
