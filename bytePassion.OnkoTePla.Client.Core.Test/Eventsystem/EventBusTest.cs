50db79d952fc13bf3c6eb933f24c292498d1d74f
xelor <kingxelor@gmail.com>
2015-05-19 12:01:50 +0200

    First Test of eventBus

---
+++ bytePassion.OnkoTePla.Client.Core.Test/Eventsystem/EventBusTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using bytePassion.OnkoTePla.Client.Core.Eventsystem;
+using bytePassion.OnkoTePla.Client.Core.Eventsystem.DomainEvents;
+using bytePassion.OnkoTePla.Contracts.Appointments;
+using Xunit;
+
+namespace bytePassion.OnkoTePla.Client.Core.Test.Eventsystem
+{
+	public class EventBusTest
+	{
+
+		private class TestEventHandler : IDomainEventHandler<AppointmentAdded>
+		{
+
+			public TestEventHandler()
+			{
+				EventHandled = false;
+			}
+
+			public void Handle(AppointmentAdded domainEvent)
+			{
+				EventHandled = true;
+			}
+
+			public bool EventHandled { private set; get; }
+		}
+
+		[Fact]
+		public void SubscribscriptionTest()
+		{
+			IEventBus eventBus = new EventBus();
+
+			var testEventHandler = new TestEventHandler();
+
+			eventBus.Subscribe(testEventHandler);
+
+			eventBus.Publish(new AppointmentAdded(new Guid(), -1, new Guid(), new Appointment(null, null, null, new DateTime(), new DateTime())));
+
+			Assert.True(testEventHandler.EventHandled);
+		}
+	}
+}
