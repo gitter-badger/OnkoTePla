891e1e29415f0f19acaccbc26f8a52d1385ca3e8
xelor <kingxelor@gmail.com>
2015-06-21 08:17:58 +0200

    Added AppointmentSet Stub and AppointmentRuleIsNotApplicableException

---
+++ bytePassion.OnkoTePla.Client.Core/Exceptions/AppointmentRuleIsNotApplicableException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace bytePassion.OnkoTePla.Client.Core.Exceptions
+{
+	public class AppointmentRuleIsNotApplicableException : Exception
+	{
+		public AppointmentRuleIsNotApplicableException()
+		{			
+		}
+
+		public AppointmentRuleIsNotApplicableException(string message)
+			: base(message)
+		{			
+		}
+
+		public AppointmentRuleIsNotApplicableException(string format, params object[] args)
+			: base(string.Format(format, args))
+		{			
+		}
+
+		public AppointmentRuleIsNotApplicableException(string message, Exception innerException)
+			: base(message, innerException)
+		{			
+		}
+
+		public AppointmentRuleIsNotApplicableException (string format, Exception innerException, params object[] args)
+			: base(string.Format(format, args), innerException)
+		{			
+		}
+	}
+}
