e25c784b7d24214a438a60c7c089ff7be3fc6430
xelor <kingxelor@gmail.com>
2015-06-27 15:35:08 +0200

    Added Appointment Grid and AppointmentView

---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/SampleData/AppointmentViewModelSampleData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.Interfaces;
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.SampleData
+{
+	public class AppointmentViewModelSampleData : IAppointmentViewModel
+	{
+		public AppointmentViewModelSampleData()
+		{
+			PatientDisplayName = "Jerry Black";
+			FrameworkElementWidth = 300;
+		}
+
+		public string PatientDisplayName    { get; private set; }
+		public double FrameworkElementWidth { get; private set; }
+	}
+}
---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.Interfaces;
+using bytePassion.OnkoTePla.Contracts.Appointments;
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels
+{
+	public class AppointmentViewModel : IAppointmentViewModel
+	{
+		private Appointment appointment;
+
+		public AppointmentViewModel(Appointment appointment)
+		{
+			this.appointment = appointment;
+		}
+
+		public string PatientDisplayName
+		{
+			get { return appointment.Patient.Name; }
+		}
+
+		public double FrameworkElementWidth { get; private set; }
+	}
+}
