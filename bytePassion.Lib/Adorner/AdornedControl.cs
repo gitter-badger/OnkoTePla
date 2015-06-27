8ecdaf268424e3f77cb9162c00109bf8005b76d1
xelor <kingxelor@gmail.com>
2015-06-27 14:10:03 +0200

    Added AdornerLib to Lib

---
+++ bytePassion.Lib/Adorner/AdornedControl.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace bytePassion.Lib.Adorner
+{
+   
+    public class AdornedControl : ContentControl
+	{
+		#region private Enum (Showstate)
+
+		private enum ShowState
+		{
+			Visible,
+			Hidden,
+			FadingIn,
+			FadingOut,
+		}
+
+		#endregion
+		
+		#region constructor and static contstructor
+
+		// TODO: eigener DataContext für den AdornerContent
+	    public AdornedControl()
+	    {
+		    adornerShowState = ShowState.Hidden;
+		    closeAdornerTimer = new DispatcherTimer();
+
+		    Focusable = false; // By default don't want 'AdornedControl' to be focusable.
+
+		    DataContextChanged += (sender, eventArgs) => UpdateAdornerContentDataContext();
+
+			closeAdornerTimer.Tick += (sender, eventArgs) =>
+			                          {
+				                          closeAdornerTimer.Stop();
+				                          FadeOutAdorner();
+			                          };
+		   	    
+			closeAdornerTimer.Interval = TimeSpan.FromSeconds(CloseAdornerTimeOut);
+        }
+
+		static AdornedControl ()
+		{
+			CommandManager.RegisterClassCommandBinding(typeof(AdornedControl), ShowAdornerCommandBinding);
+			CommandManager.RegisterClassCommandBinding(typeof(AdornedControl), FadeOutAdornerCommandBinding);
+			CommandManager.RegisterClassCommandBinding(typeof(AdornedControl), HideAdornerCommandBinding);
+			CommandManager.RegisterClassCommandBinding(typeof(AdornedControl), FadeInAdornerCommandBinding);
+		}
+
+		#endregion
+
+		#region Dependency Properties
+
+		public static readonly DependencyProperty IsAdornerVisibleProperty =
+            DependencyProperty.Register("IsAdornerVisible", 
+										typeof(bool), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(IsAdornerVisible_PropertyChanged));
+
+        public static readonly DependencyProperty AdornerContentProperty =
+            DependencyProperty.Register("AdornerContent", 
+										typeof(FrameworkElement), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(AdornerContent_PropertyChanged));
+
+        public static readonly DependencyProperty HorizontalAdornerPlacementProperty =
+            DependencyProperty.Register("HorizontalAdornerPlacement", 
+										typeof(AdornerPlacement), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(AdornerPlacement.Inside));
+
+        public static readonly DependencyProperty VerticalAdornerPlacementProperty =
+            DependencyProperty.Register("VerticalAdornerPlacement", 
+										typeof(AdornerPlacement), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(AdornerPlacement.Inside));
+
+        public static readonly DependencyProperty AdornerOffsetXProperty =
+            DependencyProperty.Register("AdornerOffsetX", 
+										typeof(double), 
+										typeof(AdornedControl));
+
+        public static readonly DependencyProperty AdornerOffsetYProperty =
+            DependencyProperty.Register("AdornerOffsetY", 
+										typeof(double), 
+										typeof(AdornedControl));
+
+        public static readonly DependencyProperty IsMouseOverShowEnabledProperty =
+            DependencyProperty.Register("IsMouseOverShowEnabled", 
+										typeof(bool), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(true, IsMouseOverShowEnabled_PropertyChanged));
+
+        public static readonly DependencyProperty FadeInTimeProperty =
+            DependencyProperty.Register("FadeInTime", 
+										typeof(double), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(0.25));
+
+        public static readonly DependencyProperty FadeOutTimeProperty =
+            DependencyProperty.Register("FadeOutTime", 
+										typeof(double), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(1.0));
+
+        public static readonly DependencyProperty CloseAdornerTimeOutProperty =
+            DependencyProperty.Register("CloseAdornerTimeOut", 
+										typeof(double), 
+										typeof(AdornedControl),
+										new FrameworkPropertyMetadata(2.0, CloseAdornerTimeOut_PropertyChanged));       
+
+        #endregion Dependency Properties
+
+		#region DependencyPropertyChangedEventHandler
+
+		private static void IsAdornerVisible_PropertyChanged (DependencyObject o, DependencyPropertyChangedEventArgs e)
+		{
+			var adornedControl = (AdornedControl)o;
+			adornedControl.ShowOrHideAdornerInternal();
+		}
+		
+		private static void IsMouseOverShowEnabled_PropertyChanged (DependencyObject o, DependencyPropertyChangedEventArgs e)
+		{
+			var adornedControl = (AdornedControl)o;					//
+			adornedControl.closeAdornerTimer.Stop();				// ???????????????????????????????
+			adornedControl.HideAdorner();							//
+		}
+		
+		private static void CloseAdornerTimeOut_PropertyChanged (DependencyObject o, DependencyPropertyChangedEventArgs e)
+		{
+			var adornedControl = (AdornedControl)o;
+			adornedControl.closeAdornerTimer.Interval = TimeSpan.FromSeconds(adornedControl.CloseAdornerTimeOut);
+		}
+		
+		private static void AdornerContent_PropertyChanged (DependencyObject o, DependencyPropertyChangedEventArgs e)
+		{
+			var adornedControl = (AdornedControl)o;
+			adornedControl.ShowOrHideAdornerInternal();
+
+			var oldAdornerContent = (FrameworkElement)e.OldValue;
+			if (oldAdornerContent != null)
+			{
+				oldAdornerContent.MouseEnter -= adornedControl.adornerContent_MouseEnter;
+				oldAdornerContent.MouseLeave -= adornedControl.adornerContent_MouseLeave;
+			}
+
+			var newAdornerContent = (FrameworkElement)e.NewValue;
+			if (newAdornerContent != null)
+			{
+				newAdornerContent.MouseEnter += adornedControl.adornerContent_MouseEnter;
+				newAdornerContent.MouseLeave += adornedControl.adornerContent_MouseLeave;
+			}
+		}
+		
+		private void adornerContent_MouseEnter (object sender, MouseEventArgs e) { MouseEnterLogic(); }		
+		private void adornerContent_MouseLeave (object sender, MouseEventArgs e) { MouseLeaveLogic(); }
+
+		#endregion
+
+		#region Commands
+
+		public static readonly RoutedCommand ShowAdornerCommand    = new RoutedCommand("ShowAdorner",    typeof(AdornedControl));
+        public static readonly RoutedCommand FadeInAdornerCommand  = new RoutedCommand("FadeInAdorner",  typeof(AdornedControl));
+        public static readonly RoutedCommand HideAdornerCommand    = new RoutedCommand("HideAdorner",    typeof(AdornedControl));
+        public static readonly RoutedCommand FadeOutAdornerCommand = new RoutedCommand("FadeOutAdorner", typeof(AdornedControl));
+
+        #endregion Commands
+
+		#region Actions (Show/Hide/FadeIn/FadeOut)
+
+		public void ShowAdorner() { IsAdornerVisible = true;  }      
+        public void HideAdorner() { IsAdornerVisible = false; }
+    
+        public void FadeInAdorner()
+        {
+            if (adornerShowState == ShowState.Visible ||
+                adornerShowState == ShowState.FadingIn)
+            {
+                // Already visible or fading in.
+                return;
+            }
+
+            ShowAdorner();
+
+            if (adornerShowState != ShowState.FadingOut)
+            {
+                adornerContentFrameworkElement.Opacity = 0.0;
+            }
+
+            DoubleAnimation doubleAnimation = new DoubleAnimation(1.0, new Duration(TimeSpan.FromSeconds(FadeInTime)));
+
+			doubleAnimation.Completed += (sender, eventArgs) => adornerShowState = ShowState.Visible;
+            doubleAnimation.Freeze();
+                
+            adornerContentFrameworkElement.BeginAnimation(OpacityProperty, doubleAnimation);
+
+            adornerShowState = ShowState.FadingIn;
+        }
+
+        
+        public void FadeOutAdorner()
+        {
+            if (adornerShowState == ShowState.FadingOut || adornerShowState == ShowState.Hidden)            
+                return;                        
+
+            DoubleAnimation fadeOutAnimation = new DoubleAnimation(0.0, new Duration(TimeSpan.FromSeconds(FadeOutTime)));
+	        fadeOutAnimation.Completed += (sender, eventArgs) =>
+	                                      {
+		                                      if (adornerShowState == ShowState.FadingOut)
+		                                      {
+			                                      // Still fading out => it wasn't aborted.
+			                                      HideAdorner();
+		                                      }
+	                                      };
+            fadeOutAnimation.Freeze();
+
+            adornerContentFrameworkElement.BeginAnimation(OpacityProperty, fadeOutAnimation);
+
+            adornerShowState = ShowState.FadingOut;
+        }
+		#endregion
+
+		#region CLR-Propertywrapper for DependencyProperties
+
+		public bool IsAdornerVisible
+        {
+            get { return (bool)GetValue(IsAdornerVisibleProperty); }
+            set { SetValue(IsAdornerVisibleProperty, value);}
+        }
+        
+        public FrameworkElement AdornerContent
+        {
+            get { return (FrameworkElement)GetValue(AdornerContentProperty); }
+            set { SetValue(AdornerContentProperty, value); }
+        }
+        
+        public AdornerPlacement HorizontalAdornerPlacement
+        {
+            get { return (AdornerPlacement)GetValue(HorizontalAdornerPlacementProperty); }
+            set { SetValue(HorizontalAdornerPlacementProperty, value); }
+        }
+        
+        public AdornerPlacement VerticalAdornerPlacement
+        {
+            get { return (AdornerPlacement)GetValue(VerticalAdornerPlacementProperty); }
+            set { SetValue(VerticalAdornerPlacementProperty, value); }
+        }
+       
+        public double AdornerOffsetX
+        {
+            get { return (double)GetValue(AdornerOffsetXProperty); }
+            set { SetValue(AdornerOffsetXProperty, value); }
+        }
+       
+        public double AdornerOffsetY
+        {
+            get { return (double)GetValue(AdornerOffsetYProperty); }
+            set { SetValue(AdornerOffsetYProperty, value); }
+        }
+        
+        public bool IsMouseOverShowEnabled
+        {
+            get { return (bool)GetValue(IsMouseOverShowEnabledProperty); }
+            set { SetValue(IsMouseOverShowEnabledProperty, value); }
+        }
+       
+        public double FadeInTime
+        {
+            get { return (double)GetValue(FadeInTimeProperty); }
+            set { SetValue(FadeInTimeProperty, value); }
+        }
+        
+        public double FadeOutTime
+        {
+            get { return (double) GetValue(FadeOutTimeProperty); }
+            set { SetValue(FadeOutTimeProperty, value); }
+        }
+        
+        public double CloseAdornerTimeOut
+        {
+            get { return (double)GetValue(CloseAdornerTimeOutProperty); }
+            set { SetValue(CloseAdornerTimeOutProperty, value); }
+        }
+       
+		#endregion
+
+		#region CommandBindings + ExecutionMethods
+		
+		private static readonly CommandBinding ShowAdornerCommandBinding    = new CommandBinding(ShowAdornerCommand,   ShowAdornerCommand_Executed);
+        private static readonly CommandBinding FadeInAdornerCommandBinding  = new CommandBinding(FadeInAdornerCommand, FadeInAdornerCommand_Executed);
+        private static readonly CommandBinding HideAdornerCommandBinding    = new CommandBinding(HideAdornerCommand,   HideAdornerCommand_Executed);
+        private static readonly CommandBinding FadeOutAdornerCommandBinding = new CommandBinding(FadeInAdornerCommand, FadeOutAdornerCommand_Executed);
+
+		
+		private static void ShowAdornerCommand_Executed (object target, ExecutedRoutedEventArgs e)
+		{
+			((AdornedControl)target).ShowAdorner();
+		}
+		
+		private static void FadeInAdornerCommand_Executed (object target, ExecutedRoutedEventArgs e)
+		{
+			((AdornedControl)target).FadeOutAdorner();
+		}
+		
+		private static void HideAdornerCommand_Executed (object target, ExecutedRoutedEventArgs e)
+		{
+			((AdornedControl)target).HideAdorner();
+		}
+		
+		private static void FadeOutAdornerCommand_Executed (object target, ExecutedRoutedEventArgs e)
+		{
+			((AdornedControl)target).FadeOutAdorner();
+		}
+
+		#endregion
+
+		#region Private Data Members
+
+		private ShowState adornerShowState;        
+        private AdornerLayer adornerLayer;      
+        private FrameworkElementAdorner adornerContentFrameworkElement;   
+     
+        private readonly DispatcherTimer closeAdornerTimer;
+        
+        #endregion
+
+        #region Private/Internal Functions               
+       
+        private void UpdateAdornerContentDataContext()
+        {
+            if (AdornerContent != null)
+            {
+                AdornerContent.DataContext = DataContext;
+            }
+        }       
+
+		private void ShowOrHideAdornerInternal ()
+		{
+			if (IsAdornerVisible)
+			{
+				CreateAndShowAdorner();
+			}
+			else
+			{
+				HideAndDestroyAdorner();
+			}
+		}
+        
+        private void CreateAndShowAdorner()
+        {
+            if (adornerContentFrameworkElement != null)            
+                return;
+            
+
+            if (AdornerContent != null)
+            {
+                if (adornerLayer == null)                
+                    adornerLayer = AdornerLayer.GetAdornerLayer(this);
+
+                if (adornerLayer == null)
+					throw new Exception();
+
+				FrameworkElement adornedControl = this; 
+
+				adornerContentFrameworkElement = new FrameworkElementAdorner(AdornerContent, adornedControl,
+																			 HorizontalAdornerPlacement, VerticalAdornerPlacement,
+																			 AdornerOffsetX, AdornerOffsetY);
+				adornerLayer.Add(adornerContentFrameworkElement);
+
+				UpdateAdornerContentDataContext();
+            }
+
+            adornerShowState = ShowState.Visible;
+        }
+       
+        private void HideAndDestroyAdorner()
+        {
+            if (adornerLayer == null || adornerContentFrameworkElement == null)           
+                return;
+                       
+            closeAdornerTimer.Stop();
+            adornerLayer.Remove(adornerContentFrameworkElement);
+            adornerContentFrameworkElement.DisconnectAdornerContent();
+
+            adornerContentFrameworkElement = null;
+            adornerLayer = null;
+           
+            adornerShowState = ShowState.Hidden;
+        }
+
+        
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            ShowOrHideAdornerInternal();
+        }
+        
+        protected override void OnMouseEnter(MouseEventArgs e) { MouseEnterLogic(); }        
+        protected override void OnMouseLeave(MouseEventArgs e) { MouseLeaveLogic(); }
+
+        private void MouseEnterLogic()
+        {
+            if (!IsMouseOverShowEnabled)           
+                return;
+            
+            closeAdornerTimer.Stop();
+            FadeInAdorner();
+        }
+    
+        private void MouseLeaveLogic()
+        {
+            if (!IsMouseOverShowEnabled)            
+                return;            
+
+            closeAdornerTimer.Start();
+        }
+     
+        #endregion
+    }
+}
---
+++ bytePassion.Lib/Adorner/FrameworkElementAdorner.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using System.Windows.Media;
+
+namespace bytePassion.Lib.Adorner
+{
+    
+    public class FrameworkElementAdorner : System.Windows.Documents.Adorner
+    {
+        
+        private readonly FrameworkElement adornerContent;
+	    private readonly FrameworkElement adornedElement;
+     
+        private readonly AdornerPlacement horizontalAdornerPlacement;
+        private readonly AdornerPlacement verticalAdornerPlacement;
+
+       
+        private readonly double offsetX;
+        private readonly double offsetY;
+               
+        
+
+        public FrameworkElementAdorner(FrameworkElement adornerContent, FrameworkElement adornedControl,
+                                       AdornerPlacement horizontalAdornerPlacement, AdornerPlacement verticalAdornerPlacement,
+                                       double offsetX, double offsetY)
+            : base(adornedControl)
+        {
+            this.adornerContent = adornerContent;
+	        this.adornedElement = (FrameworkElement)AdornedElement;
+
+            this.horizontalAdornerPlacement = horizontalAdornerPlacement;
+            this.verticalAdornerPlacement = verticalAdornerPlacement;
+            this.offsetX = offsetX;
+            this.offsetY = offsetY;
+
+			adornedControl.SizeChanged += (sender, eventArgs) => InvalidateMeasure();
+
+            AddLogicalChild(adornerContent);
+            AddVisualChild(adornerContent);
+        }           
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            adornerContent.Measure(constraint);
+            return adornerContent.DesiredSize;
+        }
+        
+        private double DetermineXPosOfAdornerContent()
+        {
+            switch (adornerContent.HorizontalAlignment)
+            {
+                case HorizontalAlignment.Left:
+                {
+                    if (horizontalAdornerPlacement == AdornerPlacement.Outside)
+                    {
+                        return -adornerContent.DesiredSize.Width + offsetX;
+                    }
+                    else
+                    {
+                        return offsetX;
+                    }
+                }
+                case HorizontalAlignment.Right:
+                {
+                    if (horizontalAdornerPlacement == AdornerPlacement.Outside)
+                    {
+                        var adornedWidth = adornedElement.ActualWidth;
+                        return adornedWidth + offsetX;
+                    }
+                    else
+                    {
+                        var adornerWidth = adornerContent.DesiredSize.Width;
+                        var adornedWidth = adornedElement.ActualWidth;
+                        var x = adornedWidth - adornerWidth;
+                        return x + offsetX;
+                    }
+                }
+                case HorizontalAlignment.Center:
+                {
+                    var adornerWidth = adornerContent.DesiredSize.Width;
+                    var adornedWidth = adornedElement.ActualWidth;
+                    var x = (adornedWidth / 2) - (adornerWidth / 2);
+                    return x + offsetX;
+                }
+                case HorizontalAlignment.Stretch:
+                {
+                    return 0.0;
+                }
+            }
+
+            return 0.0;
+        }
+        
+        private double DetermineYPosOfAdornerContent()
+        {
+            switch (adornerContent.VerticalAlignment)
+            {
+                case VerticalAlignment.Top:
+                {
+                    if (verticalAdornerPlacement == AdornerPlacement.Outside)
+                    {
+                        return -adornerContent.DesiredSize.Height + offsetY;
+                    }
+                    else
+                    {
+                        return offsetY;
+                    }
+                }
+                case VerticalAlignment.Bottom:
+                {
+                    if (verticalAdornerPlacement == AdornerPlacement.Outside)
+                    {
+                        var adornedHeight = adornedElement.ActualHeight;
+                        return adornedHeight + offsetY;
+                    }
+                    else
+                    {
+                        var adornerHeight = adornerContent.DesiredSize.Height;
+                        var adornedHeight = adornedElement.ActualHeight;
+                        var x = adornedHeight - adornerHeight;
+                        return x + offsetY;
+                    }
+                }
+                case VerticalAlignment.Center:
+                {
+                    var adornerHeight = adornerContent.DesiredSize.Height;
+                    var adornedHeight = adornedElement.ActualHeight;
+                    var x = (adornedHeight / 2) - (adornerHeight / 2);
+                    return x + offsetY;
+                }
+                case VerticalAlignment.Stretch:
+                {
+                    return 0.0;
+                }
+            }
+
+            return 0.0;
+        }
+
+       
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var x =  DetermineXPosOfAdornerContent();            
+			var y =  DetermineYPosOfAdornerContent();
+
+			var adornerWidth = adornerContent.HorizontalAlignment == HorizontalAlignment.Stretch 
+				? adornedElement.ActualWidth
+				: adornerContent.DesiredSize.Width;
+
+			var adornerHeight = adornerContent.VerticalAlignment == VerticalAlignment.Stretch 
+				? adornedElement.ActualHeight
+				: adornerContent.DesiredSize.Height;
+
+			adornerContent.Arrange(new Rect(x, y, adornerWidth, adornerHeight));
+
+            return finalSize;
+        }
+
+        protected override Int32 VisualChildrenCount
+        {
+            get { return 1; }
+        }
+
+        protected override Visual GetVisualChild(Int32 index)
+        {
+            return adornerContent;
+        }
+
+        protected override IEnumerator LogicalChildren
+        {
+            get
+            {
+				var list = new ArrayList(VisualChildrenCount)
+	                             {
+		                             adornerContent
+	                             };
+
+	            return list.GetEnumerator();
+            }
+        }
+       
+        public void DisconnectAdornerContent()
+        {
+            RemoveLogicalChild(adornerContent);
+            RemoveVisualChild(adornerContent);
+        }
+                
+    }
+}
