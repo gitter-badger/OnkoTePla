edf1563dff89ea080405cb995ac95e7d85982ba2
xelor <kingxelor@gmail.com>
2015-07-03 14:29:58 +0200

    FileRename: Added more UnitTests

---
+++ bytePassion.FileRename.Test/SpezialEndingAnalyzerTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using bytePassion.FileRename.RenameLogic.NameAnalyzer;
+using Xunit;
+
+
+namespace bytePassion.FileRename.Test
+{
+	public class SpezialEndingAnalyzerTest
+	{
+
+		[Theory]
+		[MemberData("TestDataForSpezialEndingRecognitionTest")]
+		public void SpezialEndingRecognitionTest (string name, bool containsSpezialEnding)
+		{
+			var sut = new SpecialEndingAnalyzer();
+
+			var whiteSpaceRecognized = sut.IsMatch(name);
+
+			Assert.Equal(containsSpezialEnding, whiteSpaceRecognized);
+		}
+
+		public static readonly IEnumerable<object[]> TestDataForSpezialEndingRecognitionTest = 
+			new[]
+			{
+				new object[]{ "myfile",    false  },
+				new object[]{ "f-34",      false  },
+				new object[]{ "file-2344", true   },				
+				new object[]{ "file-23aa", true   },
+				new object[]{ "file-2baa", false   },
+			};
+	}
+}
---
+++ bytePassion.FileRename.Test/TestWhiteSpaceAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using bytePassion.FileRename.RenameLogic.NameAnalyzer;
+using Xunit;
+
+
+namespace bytePassion.FileRename.Test
+{
+	public class TestWhiteSpaceAnalyzer
+	{
+
+		[Theory]
+		[MemberData("TestDataForWhiteSpaceRecognitionTest")]
+		public void WhiteSpaceRecognitionTest(string name, bool containsWhiteSpace)
+		{
+			var sut = new WhiteSpaceAnalyzer();
+
+			var whiteSpaceRecognized = sut.IsMatch(name);
+
+			Assert.Equal(containsWhiteSpace, whiteSpaceRecognized);
+		}
+
+		public static readonly IEnumerable<object[]> TestDataForWhiteSpaceRecognitionTest = 
+			new[]
+			{
+				new object[]{ "my file", true  },
+				new object[]{ " file",   true  },
+				new object[]{ "file ",   true  },
+				new object[]{ "myfile",  false }				
+			};
+
+	}
+}
