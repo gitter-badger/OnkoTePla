cd7eaac4f4c8886dd78b495bdedbc09a981f8574
xelor <kingxelor@gmail.com>
2015-07-05 18:30:06 +0200

    Added SessionInfo and UserDialogBox

---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/SessionInfo/SessionInformation.cs
@@ -0,0 +1,10 @@
+﻿using bytePassion.OnkoTePla.Contracts.Config;
+
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.SessionInfo
+{
+	public class SessionInformation
+	{
+		public User LoggedInUser { get; set; }
+	}
+}
---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/UserNotificationService/UserDialogBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.UserNotificationService
+{
+	// TODO : replace with mahapps dialog
+
+	public class UserDialogBox
+	{
+		private readonly string caption;
+		private readonly string messageText;
+		private readonly MessageBoxButton buttons;
+		private readonly MessageBoxImage image;
+
+		public UserDialogBox(string caption, string messageText, MessageBoxButton buttons, MessageBoxImage image)
+		{
+			this.caption = caption;
+			this.messageText = messageText;
+			this.buttons = buttons;
+			this.image = image;
+		}
+
+		public MessageBoxResult ShowDialog()
+		{
+			return MessageBox.Show(messageText, caption, buttons, image);
+		}
+	}
+}
