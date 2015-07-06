f47e2d8e526e218ef18c740455502600b754834d
xelor <kingxelor@gmail.com>
2015-07-06 16:46:25 +0200

    Remove old Xml-Serializer test and replaced it with JSon-Serializer-Stub

---
+++ bytePassion.OnkoTePla.Client.Core/Repositories/Patients/JSonPatientDataStore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using bytePassion.OnkoTePla.Contracts.Patients;
+
+
+namespace bytePassion.OnkoTePla.Client.Core.Repositories.Patients
+{
+	public class JSonPatientDataStore :  IPersistenceService<IEnumerable<Patient>>
+	{
+		
+		private readonly string filename;
+
+		public JSonPatientDataStore(string filename)
+		{
+			this.filename = filename;
+		}
+
+		public void Persist(IEnumerable<Patient> data)
+		{
+			// TODO: persist here
+		}
+
+		public IEnumerable<Patient> Load()
+		{
+			// TODO load here
+			return new List<Patient>();
+		}
+	}
+}
