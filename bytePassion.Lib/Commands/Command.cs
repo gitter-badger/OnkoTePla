afe1f18860c55a3e0c76b99409733af9c589963f
xelor <kingxelor@gmail.com>
2015-07-06 16:47:31 +0200

    Lib: Command extended

--- bytePassion.Lib/Commands/Command.cs
+++ bytePassion.Lib/Commands/Command.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Windows.Input;
 
-
 namespace bytePassion.Lib.Commands
 {
-	public class Command : ICommand
+	public class Command : ICommand, IDisposable
 	{
 		private readonly Func<bool> canExecute;
 		private readonly Action execute;
+		private readonly UpdateCommandInformation updateCommandInformation;
 
 
 		public Command (Action execute, Func<bool> canExecute = null)
@@ -16,7 +16,21 @@ namespace bytePassion.Lib.Commands
 			this.canExecute = canExecute;
 		}
 
-		public bool CanExecute(object parameter)
+		public Command (Action execute, Func<bool> canExecute, UpdateCommandInformation updateCommandInformation)
+		{
+			this.execute = execute;
+			this.canExecute = canExecute;
+			this.updateCommandInformation = updateCommandInformation;
+
+			updateCommandInformation.UpdateOfCanExecuteChangedRequired += CanExecuteChangedRequired;
+		}
+
+		private void CanExecuteChangedRequired (object sender, EventArgs eventArgs)
+		{
+			RaiseCanExecuteChanged();
+		}
+
+		public bool CanExecute (object parameter)
 		{
 			if (canExecute == null)
 				return true;
@@ -24,19 +38,43 @@ namespace bytePassion.Lib.Commands
 			return canExecute();
 		}
 
-		public void Execute(object parameter)
+		public void Execute (object parameter)
 		{
 			execute();
 		}
 
 		public event EventHandler CanExecuteChanged;
 
-		public void RaiseCanExecuteChanged()
+		public void RaiseCanExecuteChanged ()
 		{
 			var handler = CanExecuteChanged;
 			if (handler != null)
 				handler(this, EventArgs.Empty);
 		}
-	}
 
+		private bool disposed = false;
+		public void Dispose ()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		~Command ()
+		{
+			Dispose(false);
+		}
+
+		private void Dispose (bool disposing)
+		{
+			if (!disposed)
+				if (disposing)
+					if (updateCommandInformation != null)
+					{
+						updateCommandInformation.UpdateOfCanExecuteChangedRequired -= CanExecuteChangedRequired;
+						updateCommandInformation.Dispose();
+					}
+
+			disposed = true;
+		}
+	}
 }
--- bytePassion.Lib/Commands/ParameterrizedCommand.cs
+++ bytePassion.Lib/Commands/ParameterrizedCommand.cs
@@ -5,38 +5,79 @@ using System.Windows.Input;
 namespace bytePassion.Lib.Commands
 {
 
-	public class ParameterrizedCommand<T> : ICommand
+	public class ParameterrizedCommand<T> : ICommand, IDisposable
 	{
 		private readonly Predicate<T> canExecute;
 		private readonly Action<T> execute;
+		private readonly UpdateCommandInformation updateCommandInformation;
 
-	public ParameterrizedCommand (Action<T> execute, Predicate<T> canExecute = null)
+		public ParameterrizedCommand (Action<T> execute, Predicate<T> canExecute = null)
 		{
 			this.execute = execute;
-			this.canExecute = canExecute;	
+			this.canExecute = canExecute;
 		}
 
-		public bool CanExecute(object parameter)
-		{			
+		public ParameterrizedCommand (Action<T> execute, Predicate<T> canExecute,
+									 UpdateCommandInformation updateCommandInformation)
+		{
+			this.execute = execute;
+			this.canExecute = canExecute;
+			this.updateCommandInformation = updateCommandInformation;
+
+			updateCommandInformation.UpdateOfCanExecuteChangedRequired += CanExecuteChangedRequired;
+		}
+
+		private void CanExecuteChangedRequired (object sender, EventArgs eventArgs)
+		{
+			RaiseCanExecuteChanged();
+		}
+
+		public bool CanExecute (object parameter)
+		{
 			if (canExecute == null)
 				return true;
 
-			return canExecute((parameter == null) ? default(T) : (T) Convert.ChangeType(parameter, typeof (T)));
+			return canExecute((parameter == null) ? default(T) : (T)Convert.ChangeType(parameter, typeof(T)));
 		}
 
-		public void Execute(object parameter)
+		public void Execute (object parameter)
 		{
-			execute((parameter == null) ? default(T) : (T) Convert.ChangeType(parameter, typeof (T)));
+			execute((parameter == null) ? default(T) : (T)Convert.ChangeType(parameter, typeof(T)));
 		}
 
 		public event EventHandler CanExecuteChanged;
 
-		public void RaiseCanExecuteChanged()
+		public void RaiseCanExecuteChanged ()
 		{
 			var handler = CanExecuteChanged;
 			if (handler != null)
 				handler(this, EventArgs.Empty);
 		}
-	}
 
+		private bool disposed = false;
+		public void Dispose ()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		~ParameterrizedCommand ()
+		{
+			Dispose(false);
+		}
+
+		private void Dispose (bool disposing)
+		{
+			if (!disposed)
+				if (disposing)
+					if (updateCommandInformation != null)
+					{
+						updateCommandInformation.UpdateOfCanExecuteChangedRequired -= CanExecuteChangedRequired;
+						updateCommandInformation.Dispose();
+					}
+
+			disposed = true;
+		}
+	}
 }
+
