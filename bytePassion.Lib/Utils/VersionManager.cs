7291ba4bdc2c45d6469e487f247c9e53c951388c
xelor <kingxelor@gmail.com>
2015-07-09 17:51:28 +0200

    VersionManager improved

--- bytePassion.Lib/Utils/VersionManager.cs
+++ bytePassion.Lib/Utils/VersionManager.cs
@@ -5,133 +5,142 @@ using bytePassion.Lib.FrameworkExtensions;
 
 
 namespace bytePassion.Lib.Utils
-{	
-    public class VersionManager<T> : INotifyPropertyChanged
-    {
-	    public event EventHandler<T> CurrentVersionChanged; 
+{
+	public class VersionManager<T> : INotifyPropertyChanged
+	{
+		public event EventHandler<T> CurrentVersionChanged;
 
-	    private readonly uint maximalSavedVersions;
+		private readonly uint maximalSavedVersions;
 
 		private readonly LinkedList<T> versions;
-	    private LinkedListNode<T> currentVersionPointer;	    
+		private LinkedListNode<T> currentVersionPointer;
 
-	    private bool undoPossible;
-	    private bool redoPossible;
+		private bool undoPossible;
+		private bool redoPossible;
 
-	    public VersionManager(uint maximalSavedVersions)
-	    {
-		    this.maximalSavedVersions = maximalSavedVersions;
+		public VersionManager (uint maximalSavedVersions)
+		{
+			this.maximalSavedVersions = maximalSavedVersions;
 			versions = new LinkedList<T>();
-		    currentVersionPointer = null;
+			CurrentVersionPointer = null;
 
-		    UndoPossible = false;
-		    RedoPossible = false;
-	    }
+			CheckIfUndoAndRedoIsPossible();
+		}
 
-		public VersionManager (uint maximalSavedVersions, T initialVersion) 
+		public VersionManager (uint maximalSavedVersions, T initialVersion)
 			: this(maximalSavedVersions)
 		{
 			AddnewVersion(initialVersion);
 		}
 
-	    public void FixCurrentVersion()
-	    {
-		    lock (this)
-		    {
-			    versions.Clear();
-			    versions.AddFirst(new LinkedListNode<T>(currentVersionPointer.Value));
-			    currentVersionPointer = versions.First;
+		public void FixCurrentVersion ()
+		{
+			lock (this)
+			{
+				versions.Clear();
+				versions.AddFirst(CurrentVersionPointer);
 
 				CheckIfUndoAndRedoIsPossible();
-		    }		    
-	    }
-
-	    public bool UndoPossible
-	    {
-		    get { return undoPossible; }
-		    private set { PropertyChanged.ChangeAndNotify(this, ref undoPossible, value); }
-	    }
-	   
-	    public bool RedoPossible
-	    {
-		    get { return redoPossible; }
-			private set { PropertyChanged.ChangeAndNotify(this, ref redoPossible, value); }
-	    }		
-
-	    public void AddnewVersion(T newVersion)
-	    {
-		    lock (this)
-		    {
-			    var newVersionNode = new LinkedListNode<T>(newVersion);
-
-			    RemoveAllFromEndTo(currentVersionPointer);
-			    versions.AddLast(newVersionNode);
-			    currentVersionPointer = newVersionNode;
-
-			    if (versions.Count == maximalSavedVersions+1)
-				    versions.RemoveFirst();
-
-			    CheckIfUndoAndRedoIsPossible();
-		    }
-	    }
-
-	    public T CurrentVersion
-	    {
-		    get
-		    {
-			    lock (this)
-			    {
-				    return currentVersionPointer != null // TODO c# 6.0: .?
-						? currentVersionPointer.Value 
-						: default(T);
-			    }
-		    }
-	    }
-
-	    public void Undo()
-	    {
-		    lock (this)
-		    {
-			    if (UndoPossible)
-			    {
-				    currentVersionPointer = currentVersionPointer.Previous;
-				    CheckIfUndoAndRedoIsPossible();
-
-				    RaiseCurrentVersionChanged();
-			    } 
-				else
-					throw new InvalidOperationException("undo not possible");
-		    }
-	    }	    
-
-	    public void Redo()
-	    {
-		    lock (this)
-		    {
-			    if (RedoPossible)
-			    {
-				    currentVersionPointer = currentVersionPointer.Next;
-				    CheckIfUndoAndRedoIsPossible();
-
-					RaiseCurrentVersionChanged();
-			    }
-				else
-					throw new InvalidOperationException("redo not possible");
-		    }
-	    }
-
-		private void RaiseCurrentVersionChanged ()
-		{
-			var handler = CurrentVersionChanged;
-			if (handler != null)
-				handler(this, CurrentVersion);
+			}
 		}
 
-	    private void CheckIfUndoAndRedoIsPossible()
-	    {		   
-		    UndoPossible = currentVersionPointer.Previous != null;
-		    RedoPossible = currentVersionPointer.Next     != null;		   
-	    }
+		public bool UndoPossible
+		{
+			get { return undoPossible; }
+			private set { PropertyChanged.ChangeAndNotify(this, ref undoPossible, value); }
+		}
+
+		public bool RedoPossible
+		{
+			get { return redoPossible; }
+			private set { PropertyChanged.ChangeAndNotify(this, ref redoPossible, value); }
+		}
+
+		public void AddnewVersion (T newVersion)
+		{
+			lock (this)
+			{
+				var newVersionNode = new LinkedListNode<T>(newVersion);
+
+				RemoveAllFromEndTo(CurrentVersionPointer);
+				versions.AddLast(newVersionNode);
+				CurrentVersionPointer = newVersionNode;
+
+				if (versions.Count == maximalSavedVersions+1)
+					versions.RemoveFirst();
+			}
+		}
+
+		public T CurrentVersion
+		{
+			get
+			{
+				lock (this)
+				{
+					return CurrentVersionPointer != null // TODO c# 6.0: .?
+						? CurrentVersionPointer.Value 
+						: default(T);
+				}
+			}
+		}
+
+		private LinkedListNode<T> CurrentVersionPointer
+		{
+			get { return currentVersionPointer; }
+			set
+			{
+				if (value != currentVersionPointer)
+				{
+					currentVersionPointer = value;
+
+					CheckIfUndoAndRedoIsPossible();
+
+					if (CurrentVersionPointer != null)
+					{
+
+						var handler = CurrentVersionChanged;
+						if (handler != null)
+							handler(this, CurrentVersion);
+					}
+				}
+			}
+		}
+
+		public void Undo ()
+		{
+			lock (this)
+			{
+				if (UndoPossible)
+					CurrentVersionPointer = CurrentVersionPointer.Previous;
+				else
+					throw new InvalidOperationException("undo not possible");
+			}
+		}
+
+		public void Redo ()
+		{
+			lock (this)
+			{
+				if (RedoPossible)
+					CurrentVersionPointer = CurrentVersionPointer.Next;
+				else
+					throw new InvalidOperationException("redo not possible");
+			}
+		}
+
+		private void CheckIfUndoAndRedoIsPossible ()
+		{
+			if (CurrentVersionPointer != null)
+			{
+				UndoPossible = CurrentVersionPointer.Previous != null;
+				RedoPossible = CurrentVersionPointer.Next     != null;
+			}
+			else
+			{
+				UndoPossible = false;
+				RedoPossible = false;
+			}
+		}
 
 		private void RemoveAllFromEndTo (LinkedListNode<T> node)
 		{
@@ -139,6 +148,6 @@ namespace bytePassion.Lib.Utils
 				versions.RemoveLast();
 		}
 
-	    public event PropertyChangedEventHandler PropertyChanged;
-    }
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
 }
