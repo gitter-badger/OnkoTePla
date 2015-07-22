c8d0dbc827dc065a308a5a49d4fe26e3a26ac4e2
xelor <kingxelor@gmail.com>
2015-07-22 15:10:01 +0200

    Lib extended by Linq-Extension "Do"

---
+++ bytePassion.Lib/FrameworkExtensions/LinqExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bytePassion.Lib.FrameworkExtensions
+{
+    public static class LinqExtension
+    {
+	    public static void Do<TSource>(this IEnumerable<TSource> items, Action<TSource> action)
+	    {
+		    foreach (var item in items)
+		    {
+			    action(item);
+		    }
+	    }
+    }
+}
