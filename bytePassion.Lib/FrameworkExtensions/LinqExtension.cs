938bd8dd9ce8d9531c0e20bac8a1ec9f808567d3
Matthias Drescher <kingxelor@live.com>
2015-08-30 21:04:14 +0200

    Lib: Extended Linq-Extensions

--- bytePassion.Lib/FrameworkExtensions/LinqExtension.cs
+++ bytePassion.Lib/FrameworkExtensions/LinqExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace bytePassion.Lib.FrameworkExtensions
 {
@@ -12,5 +14,17 @@ namespace bytePassion.Lib.FrameworkExtensions
 			    action(item);
 		    }
 	    }
+
+
+	    public static ObservableCollection<TSource> ToObservableCollection<TSource>(this IEnumerable<TSource> items)
+	    {
+		    return new ObservableCollection<TSource>(items);
+	    }
+
+
+	    public static IEnumerable<TSource> Append<TSource>(this IEnumerable<TSource> items, TSource itemToAppend)
+	    {
+		    return items.Concat(new List<TSource> { itemToAppend });
+	    } 
     }
 }
