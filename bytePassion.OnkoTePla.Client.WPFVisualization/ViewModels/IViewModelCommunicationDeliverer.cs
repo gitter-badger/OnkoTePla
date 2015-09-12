acb85fbcc26a3b014279fa5da7836d4954eb68c3
Matthias Drescher <kingxelor@live.com>
2015-09-12 10:43:47 +0200

    Added Interface, so that behaviors have access to ViewModelCommunication

---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/ViewModels/IViewModelCommunicationDeliverer.cs
@@ -0,0 +1,9 @@
+﻿using bytePassion.Lib.Communication.ViewModel;
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels
+{
+	public interface IViewModelCommunicationDeliverer
+	{
+		IViewModelCommunication ViewModelCommunication { get; }
+	}
+}
