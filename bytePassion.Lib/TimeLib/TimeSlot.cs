24a8cee0b77cbae8d468fae40010e9a482c7ea9d
xelor <kingxelor@gmail.com>
2015-09-17 16:25:05 +0200

    extended timelib by timeSlot-class

---
+++ bytePassion.Lib/TimeLib/TimeSlot.cs
@@ -0,0 +1,14 @@
+﻿namespace bytePassion.Lib.TimeLib
+{
+	public class TimeSlot
+	{
+		public TimeSlot (Time begin, Time end)
+		{
+			Begin = begin;
+			End   = end;
+		}
+
+		public Time Begin { get; }
+		public Time End   { get; }
+	}
+}
