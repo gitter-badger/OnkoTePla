2067988ad7ff1a16eaf85832416df9b0b4edb4b9
Matthias Drescher <kingxelor@live.com>
2015-09-19 22:27:41 +0200

    Added Button and WIndowStub to Add an Appointemtn

---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/ViewModels/AddAppointmentDialog/AddAppointmentDialogViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+using bytePassion.Lib.WpfUtils.Commands;
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.AddAppointmentDialog
+{
+	public class AddAppointmentDialogViewModel : IAddAppointmentDialogViewModel
+	{
+
+		public AddAppointmentDialogViewModel()
+		{
+			CloseDialog = new Command(() =>
+			{
+				
+			});
+		}
+
+		public ICommand CloseDialog { get; }
+	}
+}
---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/ViewModels/AddAppointmentDialog/IAddAppointmentDialogViewModel.cs
@@ -0,0 +1,9 @@
+using System.Windows.Input;
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.AddAppointmentDialog
+{
+	public interface IAddAppointmentDialogViewModel
+	{
+		ICommand CloseDialog { get; }
+	}
+}
