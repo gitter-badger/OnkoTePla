7ef592dbc0e0ccc5842eadf9338fa287e50bb57d
xelor <kingxelor@gmail.com>
2015-09-21 14:46:22 +0200

    The birthday is played on the main grid along with the name (this should be optional, and ajustable on the options page)

--- bytePassion.OnkoTePla.Client.WPFVisualization/ViewModels/AppointmentView/AppointmentViewModel.cs
+++ bytePassion.OnkoTePla.Client.WPFVisualization/ViewModels/AppointmentView/AppointmentViewModel.cs
@@ -217,8 +217,8 @@ namespace bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.AppointmentVi
 		}
 
 
-		public string PatientDisplayName => appointment.Patient.Name;
-		public string TimeSpan           => $"{appointment.StartTime.ToString().Substring(0, 5)} - {appointment.EndTime.ToString().Substring(0, 5)}";
+		public string PatientDisplayName => $"{appointment.Patient.Name} (*{appointment.Patient.Birthday.Year})";
+        public string TimeSpan           => $"{appointment.StartTime.ToString().Substring(0, 5)} - {appointment.EndTime.ToString().Substring(0, 5)}";
 		public string AppointmentDate    => appointment.Day.ToString();
 		public string Description        => appointment.Description;
 		public string Room               => appointment.TherapyPlace.Name;		
