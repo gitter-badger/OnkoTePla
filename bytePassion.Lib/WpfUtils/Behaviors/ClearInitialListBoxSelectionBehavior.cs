8ec6c2c60e1e8cbf1287d67f32e8006cf45c97ad
Matthias Drescher <kingxelor@live.com>
2015-10-04 10:51:44 +0200

    added behavior to patientSelectorListBox (not working as intended)

--- bytePassion.Lib/WpfUtils/Behaviors/ClearInitialListBoxSelectionBehavior.cs
+++ bytePassion.Lib/WpfUtils/Behaviors/ClearInitialListBoxSelectionBehavior.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interactivity;
 
@@ -26,7 +21,7 @@ namespace bytePassion.Lib.WpfUtils.Behaviors
 
 		private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
 		{
-			AssociatedObject.SelectedIndex = -1;
+			AssociatedObject.SelectedItem = null;
 		}
 	}
 }
