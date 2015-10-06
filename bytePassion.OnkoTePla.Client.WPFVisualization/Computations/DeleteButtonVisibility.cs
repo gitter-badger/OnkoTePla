b47b44b3f87f7d0ac9325038f26f1b4873d66f5c
xelor <kingxelor@gmail.com>
2015-10-06 13:59:23 +0200

    delete-Button is hidden while drag-drop

---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/Computations/DeleteButtonVisibility.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using bytePassion.Lib.WpfUtils.ConverterBase;
+using bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.AppointmentView.Helper;
+
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.Computations
+{
+	public class DeleteButtonVisibility : GenericTwoToOneValueConverter<OperatingMode, bool, Visibility>
+	{
+		protected override Visibility Convert(OperatingMode operatingMode, bool showDisabledOverlay, CultureInfo culture)
+		{
+			if (showDisabledOverlay)
+				return Visibility.Collapsed;
+
+			return operatingMode == OperatingMode.Edit ? Visibility.Visible : Visibility.Collapsed;
+		}
+	}
+}
