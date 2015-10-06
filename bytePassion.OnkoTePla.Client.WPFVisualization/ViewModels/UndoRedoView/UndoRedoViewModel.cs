1172fc49bf2bfb162bb3609a574440c7d4428f23
xelor <kingxelor@gmail.com>
2015-10-06 15:08:18 +0200

    forgot to commit

---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/ViewModels/UndoRedoView/UndoRedoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+using bytePassion.Lib.Communication.ViewModel;
+using bytePassion.Lib.WpfUtils.Commands;
+
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.UndoRedoView
+{
+	public class UndoRedoViewModel : IUndoRedoViewModel
+	{
+		private readonly IViewModelCommunication viewModelCommunication;
+
+		public UndoRedoViewModel(IViewModelCommunication viewModelCommunication)
+		{
+			this.viewModelCommunication = viewModelCommunication;
+			Undo = new Command(DoUndo);
+			Redo = new Command(DoRedo);
+		}
+
+		private void DoRedo()
+		{
+			// TODO
+		}
+
+		private void DoUndo()
+		{
+			// TODO
+		}
+
+		public ICommand Undo { get; }
+		public ICommand Redo { get; }
+	}
+
+}
