588810a08b2a1cd30fa90c037b21c1ae21c069b7
Matthias Drescher <kingxelor@live.com>
2015-10-10 23:09:24 +0200

    and more serializationDoubles

---
+++ bytePassion.OnkoTePla.Client.Core/Repositories/SerializationDoubles/UserSerializationDouble.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using bytePassion.OnkoTePla.Contracts.Config;
+
+namespace bytePassion.OnkoTePla.Client.Core.Repositories.SerializationDoubles
+{
+	public class UserSerializationDouble
+	{
+		public UserSerializationDouble()
+		{			
+		}
+
+		public UserSerializationDouble(User user)
+		{
+			ListOfAccessableMedicalPractices = user.ListOfAccessableMedicalPractices;
+			Name     = user.Name;
+			Password = user.Password;
+			Id       = user.Id;
+		}
+
+		public IReadOnlyList<Guid> ListOfAccessableMedicalPractices { get; set; }
+		public string              Name                             { get; set; }
+		public string              Password                         { get; set; }
+		public Guid                Id                               { get; set; }
+
+		public User GetUser()
+		{
+			return new User(Name, ListOfAccessableMedicalPractices, Password, Id);
+		}
+	}
+}
