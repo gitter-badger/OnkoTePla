affa57c00dcd076111fe76b528ec15830e5a0979
Alexander Horn <alexomanie@gmail.com>
2015-10-13 22:42:50 +0200

    delete-button of appoinment changes position to bottom if no space is available at top. Problem: Renderorder of grid items: the button is under the next therapyplacerow.

---
+++ bytePassion.OnkoTePla.Client.WPFVisualization/Behaviors/DeleteButtonPlacementBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using bytePassion.Lib.FrameworkExtensions;
+
+namespace bytePassion.OnkoTePla.Client.WPFVisualization.Behaviors
+{
+    public class DeleteButtonPlacementBehavior : Behavior<Button>
+    {
+        public static readonly DependencyProperty ReferenceElementProperty = DependencyProperty.Register(
+            "ReferenceElement", typeof (UIElement), typeof (DeleteButtonPlacementBehavior), new PropertyMetadata(default(UIElement)));
+
+        private int zindex;
+
+        public UIElement ReferenceElement
+        {
+            get { return (UIElement) GetValue(ReferenceElementProperty); }
+            set { SetValue(ReferenceElementProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.IsVisibleChanged += OnLayoutUpdate;
+            zindex = Canvas.GetZIndex(AssociatedObject);
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.IsVisibleChanged -= OnLayoutUpdate;
+        }
+
+        private void OnLayoutUpdate(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if(!ReferenceElement.IsUserVisible())
+            {
+                AssociatedObject.Margin = new Thickness(0, 30, 0, -25);
+            }
+            else
+            {
+                AssociatedObject.Margin = new Thickness(0, -25,0, 30); 
+            }
+        }
+    }
+}
