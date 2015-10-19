4cd8334c56b37da21c4c2263865742d27690b26a
xelor <kingxelor@gmail.com>
2015-10-19 12:52:06 +0200

    just a test-commit

--- bytePassion.OnkoTePla.Client.WPFVisualization/ViewModelMessageHandler/RejectChangesMessageHandler.cs
+++ bytePassion.OnkoTePla.Client.WPFVisualization/ViewModelMessageHandler/RejectChangesMessageHandler.cs
@@ -1,16 +1,16 @@
-﻿using System.Windows;
-using bytePassion.Lib.Communication.State;
+﻿using bytePassion.Lib.Communication.State;
 using bytePassion.Lib.Communication.ViewModel;
 using bytePassion.Lib.Communication.ViewModel.Messages;
 using bytePassion.OnkoTePla.Client.WPFVisualization.UserNotificationService;
 using bytePassion.OnkoTePla.Client.WPFVisualization.ViewModelMessages;
 using bytePassion.OnkoTePla.Client.WPFVisualization.ViewModels.AppointmentView.Helper;
 using MahApps.Metro.Controls.Dialogs;
+using System.Windows;
 using static bytePassion.OnkoTePla.Client.WPFVisualization.Global.Constants;
 
 namespace bytePassion.OnkoTePla.Client.WPFVisualization.ViewModelMessageHandler
 {
-	public class RejectChangesMessageHandler : IViewModelMessageHandler<RejectChanges>
+    public class RejectChangesMessageHandler : IViewModelMessageHandler<RejectChanges>
 	{
 		private readonly IViewModelCommunication viewModelCommunication;
 		private readonly IGlobalState<AppointmentModifications> currentModifiedAppointmentVariable;
