3f8eeebdc98186d2b0aa6627d6f2018fc7577e5b
xelor <kingxelor@gmail.com>
2015-10-29 16:22:44 +0100

    forgot to add ...

---
+++ bytePassion.Lib/Types/SemanticTypes/Height.cs
@@ -0,0 +1,14 @@
+﻿using bytePassion.Lib.Types.SemanticTypes.Base;
+
+
+namespace bytePassion.Lib.Types.SemanticTypes
+{
+
+    public class Height : SimpleDoubleSemanticType
+    {
+        public Height(double value)
+            : base(value) 
+        {
+        }
+    }
+}
