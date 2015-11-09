f0f4f66d13e7c71e6ac4469eefe2a4c4d7453ce7
Matthias Drescher <kingxelor@live.com>
2015-11-09 12:41:50 +0100

    lib: guard extended by multiple-argumentNotNull + refactoring

--- bytePassion.Lib/Utils/Guard.cs
+++ bytePassion.Lib/Utils/Guard.cs
@@ -1,79 +1,96 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using bytePassion.Lib.FrameworkExtensions;
 
 namespace bytePassion.Lib.Utils
 {
 	public static class Guard
 	{
-						
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+		/////////                                                                                   ///////////
+		/////////                                   Not null                                        ///////////
+		/////////                                                                                   ///////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		public static void ArgumentNotNull (object argumentValue)
 		{
 			if (argumentValue == null)
 			{				
-				Type valueType = argumentValue.GetType();									
+				var valueType = argumentValue.GetType();									
 				throw new ArgumentNullException(valueType.Name);
 			}
 		}
-			
-							
-		public static void KeyInDictionary (IDictionary dictionary, object key)
+
+		public static void ArgumentNotNull(params object[] argumentValues)
 		{
-			ArgumentNotNull(key);
-			ArgumentNotNull(dictionary);
-
-			if (!dictionary.Contains(key))
-			{
-				Type valueType = dictionary.GetType();
-				throw new KeyNotFoundException($"{key} is not found in {valueType.Name}");
-			}
-		}
-		
-
-		public static void KeyNotInDictionary (IDictionary dictionary, object key)
-		{
-			ArgumentNotNull(key);
-			ArgumentNotNull(dictionary);
-
-			if (dictionary.Contains(key))
-			{
-				Type valueType = dictionary.GetType();
-				throw new ArgumentException($"{key} is already defined in {valueType.Name}");				
-			}
+			argumentValues.Do(ArgumentNotNull);
 		}
 
 
-		public static void KeyInDictionary<TKey, TValue> (IDictionary<TKey, TValue> dictionary, TKey key)
-		{
-			ArgumentNotNull(key);
-			ArgumentNotNull(dictionary);
-
-			if (!dictionary.ContainsKey(key))
-			{
-				Type valueType = dictionary.GetType();
-				throw new KeyNotFoundException($"{key} is not found in {valueType.Name}");
-			}
-		}
-
-
-		public static void KeyNotInDictionary<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key)
-		{
-			ArgumentNotNull(key);
-			ArgumentNotNull(dictionary);
-
-			if (dictionary.ContainsKey(key))
-			{
-				Type valueType = dictionary.GetType();
-				throw new ArgumentException($"{key} is already defined in {valueType.Name}");
-			}
-		}
-		
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+		/////////                                                                                   ///////////
+		/////////                                   casting                                         ///////////
+		/////////                                                                                   ///////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		public static T SafeCast<T>(object instance) where T : class
 		{
 			var instanceAsT = instance as T;
 			ArgumentNotNull(instanceAsT);
 			return instanceAsT;
+		}
+
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+		/////////                                                                                   ///////////
+		/////////                                 Dictionaries                                      ///////////
+		/////////                                                                                   ///////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		public static void KeyInDictionary (IDictionary dictionary, object key)
+		{
+			ArgumentNotNull(key, dictionary);			
+
+			if (!dictionary.Contains(key))
+			{
+				var valueType = dictionary.GetType();
+				throw new KeyNotFoundException($"{key} is not found in {valueType.Name}");
+			}
+		}
+		
+		public static void KeyNotInDictionary (IDictionary dictionary, object key)
+		{
+			ArgumentNotNull(key, dictionary);
+
+			if (dictionary.Contains(key))
+			{
+				var valueType = dictionary.GetType();
+				throw new ArgumentException($"{key} is already defined in {valueType.Name}");				
+			}
+		}
+
+		public static void KeyInDictionary<TKey, TValue> (IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			ArgumentNotNull(key, dictionary);
+
+			if (!dictionary.ContainsKey(key))
+			{
+				var valueType = dictionary.GetType();
+				throw new KeyNotFoundException($"{key} is not found in {valueType.Name}");
+			}
+		}
+
+		public static void KeyNotInDictionary<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			ArgumentNotNull(key, dictionary);
+
+			if (dictionary.ContainsKey(key))
+			{
+				var valueType = dictionary.GetType();
+				throw new ArgumentException($"{key} is already defined in {valueType.Name}");
+			}
 		}		
 	}
 }
