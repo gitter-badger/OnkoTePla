f9c5de8f214a75271a4f95e2040dabdcd020d04b
xelor <kingxelor@gmail.com>
2015-12-03 10:21:17 +0100

    lib: added InvertedAngleToDoubleConverter

---
+++ bytePassion.Lib/WpfLib/Converter/InvertedAngleToDoubleConverter.cs
@@ -0,0 +1,22 @@
+using bytePassion.Lib.GeometryLib.Base;
+using bytePassion.Lib.Types.SemanticTypes;
+using bytePassion.Lib.WpfLib.ConverterBase;
+using System.Globalization;
+
+
+namespace bytePassion.Lib.WpfLib.Converter
+{
+
+    public class InvertedAngleToDoubleConverter : GenericValueConverter<Angle, double>
+    {
+        protected override double Convert(Angle angle, CultureInfo culture)
+        {
+            return angle.Inverted.Value;
+        }
+
+        protected override Angle ConvertBack(double value, CultureInfo culture)
+        {
+            return new Angle(new Degree(value)).Inverted;
+        }
+    }
+}
