6ad28def4845ed3fef186be1e1592adba463c725
xelor <kingxelor@gmail.com>
2015-12-16 10:42:23 +0100

    renamed some projects

--- bytePassion.OnkoTePla.Client.WpfUi/Properties/Resources.Designer.cs
+++ bytePassion.OnkoTePla.Client.WpfUi/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace bytePassion.OnkoTePla.Client.WPFVisualization.Properties {
+namespace bytePassion.OnkoTePla.Client.WpfUi.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace bytePassion.OnkoTePla.Client.WPFVisualization.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("bytePassion.OnkoTePla.Client.WPFVisualization.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("bytePassion.OnkoTePla.Client.WpfUi.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- bytePassion.OnkoTePla.SampleDataCreation/Properties/Settings.Designer.cs
+++ bytePassion.OnkoTePla.SampleDataCreation/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace bytePassion.OnkoTePla.Client.WPFVisualization.Properties {
+namespace bytePassion.OnkoTePla.SampleDataCreation.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
