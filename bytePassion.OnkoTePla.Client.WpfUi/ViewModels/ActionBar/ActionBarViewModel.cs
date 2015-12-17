302f043dcb74da38dc47305a7c79942cd84904d3
xelor <kingxelor@gmail.com>
2015-12-17 17:56:45 +0100

    client-UI: forgotToCommit

---
+++ bytePassion.OnkoTePla.Client.WpfUi/ViewModels/ActionBar/ActionBarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using bytePassion.OnkoTePla.Client.WpfUi.ViewModels.ConnectionStatusView;
+using System.ComponentModel;
+using System.Windows.Input;
+
+
+namespace bytePassion.OnkoTePla.Client.WpfUi.ViewModels.ActionBar
+{
+    internal class ActionBarViewModel : ViewModel, IActionBarViewModel
+    {
+        public ActionBarViewModel(IConnectionStatusViewModel connectionStatusViewModel)
+        {
+            ConnectionStatusViewModel = connectionStatusViewModel;
+        }
+
+        public ICommand ShowOverview { get; }
+        public ICommand ShowSearch   { get; }
+        public ICommand ShowSettings { get; }
+        public ICommand Logout       { get; }
+        public ICommand ShowAbout    { get; }
+
+        public IConnectionStatusViewModel ConnectionStatusViewModel { get; }
+
+        protected override void CleanUp()
+        {
+        }
+        public override event PropertyChangedEventHandler PropertyChanged;
+    }
+}
