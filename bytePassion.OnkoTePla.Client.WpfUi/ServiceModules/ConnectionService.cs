30a8b88a766eb0c493c673d92b116f725ba00972
xelor <kingxelor@gmail.com>
2015-12-18 13:20:10 +0100

    client-UI: added ConnectionService-Stub

---
+++ bytePassion.OnkoTePla.Client.WpfUi/ServiceModules/ConnectionService.cs
@@ -0,0 +1,33 @@
+﻿using bytePassion.Lib.Types.Communication;
+using System;
+
+
+namespace bytePassion.OnkoTePla.Client.WpfUi.ServiceModules
+{
+    internal class ConnectionService
+    {
+        public event EventHandler<EventArgs> ConnectionStatusChanged;
+
+        private readonly Address ServerAddress;        
+
+        public ConnectionService(Address serverAddress)
+        {
+            ServerAddress = serverAddress;
+            IsConnected = false;
+        }
+
+        public bool IsConnected { get; }
+
+        public void TryConnect()
+        {
+            
+            ConnectionStatusChanged?.Invoke(this, new EventArgs());
+        }
+
+        public void TryDisconnect()
+        {
+
+            ConnectionStatusChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
