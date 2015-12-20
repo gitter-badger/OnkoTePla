d32af1f520a113765ba92315d20ce42cbfed6a81
Matthias Drescher <kingxelor@live.com>
2015-12-20 20:28:13 +0100

    client-UI: first implementation of Login-View

--- bytePassion.OnkoTePla.Client.WpfUi/ViewModels/LoginView/ILoginViewModel.cs
+++ bytePassion.OnkoTePla.Client.WpfUi/ViewModels/LoginView/ILoginViewModel.cs
@@ -1,7 +1,21 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
 namespace bytePassion.OnkoTePla.Client.WpfUi.ViewModels.LoginView
 {
-    internal interface ILoginViewModel : IViewModel
+	internal interface ILoginViewModel : IViewModel
     {
-        
+        ICommand Login      { get; }
+		ICommand Connect    { get; }
+		ICommand Disconnect { get; }
+
+		ObservableCollection<string> AvailableUsers { get; }
+
+		string SelectedUserName { get; set; }
+
+		string Password { set; }
+		string ServerAddress { get; set; }
+
+		bool AutoConnectOnNextStart { get; set; }
     }
 }
--- bytePassion.OnkoTePla.Client.WpfUi/ViewModels/LoginView/LoginViewModel.cs
+++ bytePassion.OnkoTePla.Client.WpfUi/ViewModels/LoginView/LoginViewModel.cs
@@ -1,19 +1,95 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using bytePassion.Lib.FrameworkExtensions;
+using bytePassion.Lib.Types.Communication;
+using bytePassion.Lib.WpfLib.Commands;
 using bytePassion.OnkoTePla.Client.DataAndService.SessionInfo;
+using bytePassion.OnkoTePla.Client.Resources;
+using bytePassion.OnkoTePla.Client.WpfUi.UserNotificationService;
+using bytePassion.OnkoTePla.Contracts.Config;
 
 namespace bytePassion.OnkoTePla.Client.WpfUi.ViewModels.LoginView
 {
 	internal class LoginViewModel : ViewModel, 
                                     ILoginViewModel
-    {
-	    private readonly ISession session;
+	{
+		private static readonly Protocol Protocol = new TcpIpProtocol();
 
-	    public LoginViewModel(ISession session)
+		private readonly ISession session;
+		private string selectedUserName;
+		
+		public LoginViewModel(ISession session)
 	    {
 		    this.session = session;
+
+			Login = new Command(DoLogin);
+			Connect = new Command(DoConnect);
+			Disconnect = new Command(DoDisconnect);
+
+			AvailableUsers = session.AvailableUsers
+									.Select(user => user.Name)
+									.ToObservableCollection();
+
+			session.UserListAvailable += OnNewUserListAvailable;
 	    }
 
-	    protected override void CleanUp() { }
-        public override event PropertyChangedEventHandler PropertyChanged;
-    }
+		private void OnNewUserListAvailable(IReadOnlyList<User> newUserList)
+		{
+			AvailableUsers.Clear();
+
+			newUserList.Select(user => user.Name)
+					   .Do(AvailableUsers.Add);
+		}
+
+		private void DoDisconnect()
+		{
+			session.TryDisconnect();
+		}
+
+		private async void DoConnect()
+		{
+			if (IpV4AddressIdentifier.IsIpV4Address(ServerAddress + ":" + GlobalConstants.TcpIpPort))
+			{
+				session.TryConnect(new Address(Protocol, IpV4AddressIdentifier.Parse(ServerAddress + ":" + GlobalConstants.TcpIpPort)));
+			}
+			else
+			{
+				var dialog = new UserDialogBox("", $"{ServerAddress} ist keine gültige Ip-Adresse",
+											   MessageBoxButton.OK, MessageBoxImage.Error);
+				await dialog.ShowMahAppsDialog();
+			}
+		}
+
+		private void DoLogin()
+		{
+			var selectedUser = session.AvailableUsers.First(user => user.Name == SelectedUserName);
+
+			session.TryLogin(selectedUser, Password);
+		}
+
+
+		public ICommand Login      { get; }
+		public ICommand Connect    { get; }
+		public ICommand Disconnect { get; }
+
+		public ObservableCollection<string> AvailableUsers { get; }
+
+		public string SelectedUserName
+		{
+			get { return selectedUserName; }
+			set { PropertyChanged.ChangeAndNotify(this, ref selectedUserName, value); }
+		}
+
+		public string Password { private get; set; }
+		public string ServerAddress { get; set; }
+
+		public bool AutoConnectOnNextStart { get; set; }
+
+		protected override void CleanUp () { }
+		public override event PropertyChangedEventHandler PropertyChanged;
+	}
 }
