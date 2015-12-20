d212c63fe40393a9f8d8064aaa3b983fdd5a52cc
Matthias Drescher <kingxelor@live.com>
2015-12-20 16:13:11 +0100

    added  testCode to get Local ip-Adress

--- bytePassion.OnkoTePla.Client.WpfUi/ViewModels/ActionBar/ActionBarViewModel.cs
+++ bytePassion.OnkoTePla.Client.WpfUi/ViewModels/ActionBar/ActionBarViewModel.cs
@@ -1,17 +1,17 @@
-﻿using bytePassion.Lib.Communication.ViewModel;
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using bytePassion.Lib.Communication.ViewModel;
 using bytePassion.Lib.Utils;
 using bytePassion.Lib.WpfLib.Commands;
 using bytePassion.OnkoTePla.Client.WpfUi.Enums;
 using bytePassion.OnkoTePla.Client.WpfUi.ViewModelMessages;
 using bytePassion.OnkoTePla.Client.WpfUi.ViewModels.ConnectionStatusView;
-using System.ComponentModel;
-using System.Windows.Input;
 
 #pragma warning disable 0067
 
 namespace bytePassion.OnkoTePla.Client.WpfUi.ViewModels.ActionBar
 {
-    internal class ActionBarViewModel : ViewModel, IActionBarViewModel
+	internal class ActionBarViewModel : ViewModel, IActionBarViewModel
     {
         private readonly IViewModelCommunication viewModelCommunication;
         private readonly IWindowBuilder<Views.AboutDialog> dialogBuilder;
@@ -29,9 +29,41 @@ namespace bytePassion.OnkoTePla.Client.WpfUi.ViewModels.ActionBar
             ShowOptions  = new Command(() => viewModelCommunication.Send(new ShowPage(MainPage.Options)));
 
             ShowAbout = new Command(ShowAboutDialog);
+			Logout = new Command(DoLogOut);
         }
 
-        private void ShowAboutDialog()
+	    private static void DoLogOut()
+	    {
+			//			var host = Dns.GetHostEntry(Dns.GetHostName());
+			//			foreach (var ip in host.AddressList)
+			//			{
+			//				if (ip.AddressFamily == AddressFamily.InterNetwork)
+			//				{
+			//					MessageBox.Show(ip.ToString());
+			//					return;
+			//				}
+			//			}
+			//
+			//			MessageBox.Show("fehler");
+
+//			// Get host name
+//			string strHostName = Dns.GetHostName();
+//
+//			// Find host by name
+//			IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+//
+//		    string result = "blubb\n";
+//
+//			// Enumerate IP addresses
+//			foreach (IPAddress ipaddress in iphostentry.AddressList)
+//			{
+//				result += ipaddress.ToString() + "\n";
+//			}
+//
+//		    MessageBox.Show(result);
+	    }
+
+	    private void ShowAboutDialog()
         {
             viewModelCommunication.Send(new ShowDisabledOverlay());
 
