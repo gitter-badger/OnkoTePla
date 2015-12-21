4895d980892550e55874cc7ddb0745e96c174046
Matthias Drescher <kingxelor@live.com>
2015-12-21 20:40:25 +0100

    added IpAddressCatcher

---
+++ bytePassion.OnkoTePla.Resources/ZqmUtils/IpAddressCatcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using bytePassion.Lib.Types.Communication;
+
+namespace bytePassion.OnkoTePla.Resources.ZqmUtils
+{
+	public static class IpAddressCatcher
+	{
+		public static IReadOnlyList<Address> GetAllAvailableLocalIpAddresses ()
+		{
+			string strHostName = Dns.GetHostName();
+			var iphostentry = Dns.GetHostByName(strHostName);
+
+			var addressList = new List<Address>();
+
+			foreach (var ipaddress in iphostentry.AddressList)
+			{
+				addressList.Add(new Address(new TcpIpProtocol(),
+											AddressIdentifier.GetIpAddressIdentifierFromString(ipaddress.ToString())));
+			}
+
+			if (addressList.Count == 0)
+				addressList.Add(new Address(new TcpIpProtocol(), new IpV4AddressIdentifier(127, 0, 0, 1)));
+
+			return addressList;
+		}
+	}
+}
