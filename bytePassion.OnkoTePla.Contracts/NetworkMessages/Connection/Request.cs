bdbbfe40fefdf748e52248ca7e21822dad58c8ee
Matthias Drescher <kingxelor@live.com>
2015-12-21 21:19:39 +0100

    contracts: added NetrworkMessages for connection establishment

---
+++ bytePassion.OnkoTePla.Contracts/NetworkMessages/Connection/Request.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using bytePassion.Lib.Types.Communication;
+
+namespace bytePassion.OnkoTePla.Contracts.NetworkMessages.Connection
+{
+	public class Request
+	{
+		public Request(AddressIdentifier clientAddress)
+		{
+			ClientAddress = clientAddress;
+		}
+
+		public AddressIdentifier ClientAddress { get; }
+
+		public string AsString()
+		{
+			return $"ConnectionRequest;{ClientAddress}";
+		}
+
+		public static Request Parse(string s)
+		{
+			var parts = s.Split(';');
+
+			if (parts.Length != 2)
+				throw new ArgumentException($"{s} is not a connectionRequest");
+
+			if (parts[0] != "ConnectionRequest")
+				throw new ArgumentException($"{s} is not a connectionRequest");
+
+			var clientAddress = AddressIdentifier.GetIpAddressIdentifierFromString(parts[1]);
+
+			return new Request(clientAddress);
+		}
+	}
+}
---
+++ bytePassion.OnkoTePla.Contracts/NetworkMessages/Connection/Response.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using bytePassion.OnkoTePla.Contracts.Types;
+
+namespace bytePassion.OnkoTePla.Contracts.NetworkMessages.Connection
+{
+	public class Response
+	{
+		public Response (ConnectionSessionId sessionId)
+		{
+			SessionId = sessionId;
+		}
+
+		public ConnectionSessionId SessionId { get; }
+
+		public string AsString ()
+		{
+			return $"Confirmed;{SessionId}";
+		}
+
+		public static Response Parse (string s)
+		{
+			var parts = s.Split(';');
+
+			if (parts.Length != 2)
+				throw new ArgumentException($"{s} is not a connectionResponse");
+
+			if (parts[0] != "Confirmed")
+				throw new ArgumentException($"{s} is not a connectionResponse");
+
+			var sessionId = new ConnectionSessionId(Guid.Parse(parts[1]));
+
+			return new Response(sessionId);
+		}
+}
