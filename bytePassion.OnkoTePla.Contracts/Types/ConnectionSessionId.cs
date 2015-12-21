37ca65fcc7d0f0be7492670eddc128e582fcd318
Matthias Drescher <kingxelor@live.com>
2015-12-21 12:47:23 +0100

    contracts: added ConnectionSessionId

---
+++ bytePassion.OnkoTePla.Contracts/Types/ConnectionSessionId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using bytePassion.Lib.Types.SemanticTypes.Base;
+
+namespace bytePassion.OnkoTePla.Contracts.Types
+{
+	public class ConnectionSessionId : SemanticType<Guid>
+	{
+		public ConnectionSessionId(Guid value) : base(value, "")
+		{
+		}
+		
+		protected override Func<SemanticType<Guid>, SemanticType<Guid>, bool> EqualsFunc => (id1, id2) => id1.Value == id2.Value;
+		protected override string StringRepresentation => Value.ToString();	
+	}
+}
---
+++ bytePassion.OnkoTePla.Server.DataAndService/Data/DataCenter.cs
@@ -0,0 +1,14 @@
+﻿using bytePassion.Lib.Types.Communication;
+
+namespace bytePassion.OnkoTePla.Server.DataAndService.Data
+{
+	internal class DataCenter : IDataCenter
+	{
+		public Address ServerAddress { get; }
+	}
+
+	public interface IDataCenter
+	{
+		Address ServerAddress { get; }
+	}
+}
