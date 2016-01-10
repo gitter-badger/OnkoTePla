20e777f9e954814f6e4c65fa772f79865e2f5dc8
Matthias Drescher <kingxelor@live.com>
2016-01-10 17:48:58 +0100

    forgot to commit

---
+++ bytePassion.OnkoTePla.Server.WpfUi/ViewModels/UserPage/Helper/MedPracticeListItemData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace bytePassion.OnkoTePla.Server.WpfUi.ViewModels.UserPage.Helper
+{
+	internal class MedPracticeListItemData
+	{
+		public MedPracticeListItemData(bool isSelected, string name, Guid id)
+		{
+			IsSelected = isSelected;
+			Name = name;
+			Id = id;
+		}
+
+		public bool IsSelected { get; set; }
+		public string Name { get; }
+		public Guid Id { get; }
+	}
+}
