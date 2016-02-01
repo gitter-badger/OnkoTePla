9a55db9ae5c11f1c8c1ec3cf70b4967b08d9bbfd
Alexander Horn <alexomanie@gmail.com>
2016-02-01 22:40:42 +0100

    initial check in for metadata skeleton.

---
+++ bytePassion.OnkoTePla.Core/Repositories/StreamManagement/PracticeMetaData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using bytePassion.Lib.TimeLib;
+
+namespace bytePassion.OnkoTePla.Core.Repositories.StreamManagement
+{
+    public class PracticeMetaData
+    {
+        public Date FirstAppointmentDate { get; set; }
+        public Date LastAppointmentDate { get; set; }
+
+        public Dictionary<Guid, List<Date>> AppointmentsForPatient { get; set; }
+        public Dictionary<Date, bool> AppointmentExistenceIndex { get; set; } 
+    }
+}
---
+++ bytePassion.OnkoTePla.Core/Repositories/StreamManagement/StreamMetaDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bytePassion.OnkoTePla.Core.Repositories.StreamManagement
+{
+    public class StreamMetaDataService : IStreamMetaDataService
+    {
+        public void ReadMetaData(Guid practiceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveMetaData(Guid practiceId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
