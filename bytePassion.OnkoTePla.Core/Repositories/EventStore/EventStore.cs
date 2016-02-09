a8fd72e12fbda16ff0cf2f3801615cdce775b37f
Matthias Drescher <kingxelor@live.com>
2016-02-09 17:29:00 +0100

    just for testing: reverted to old eventstore

--- bytePassion.OnkoTePla.Core/Repositories/EventStore/EventStore.cs
+++ bytePassion.OnkoTePla.Core/Repositories/EventStore/EventStore.cs
@@ -1,58 +1,57 @@
-﻿using bytePassion.Lib.Types.Repository;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using bytePassion.Lib.Types.Repository;
 using bytePassion.OnkoTePla.Core.Domain;
 using bytePassion.OnkoTePla.Core.Eventsystem;
 using bytePassion.OnkoTePla.Core.Repositories.Config;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using bytePassion.OnkoTePla.Core.Repositories.StreamManagement;
 
 
 namespace bytePassion.OnkoTePla.Core.Repositories.EventStore
 {
-    public class EventStore : IEventStore
+	public class EventStore : IEventStore
 	{
 		private readonly IPersistenceService<IEnumerable<EventStream<AggregateIdentifier>>> persistenceService;
-        private readonly StreamManagementService streamManager;
+        //private readonly StreamManagementService streamManager;
 
         private IList<EventStream<AggregateIdentifier>> eventStreams;
 		private readonly IConfigurationReadRepository config;
 
-		public EventStore (IPersistenceService<IEnumerable<EventStream<AggregateIdentifier>>> persistenceService, StreamManagementService streamManager, IConfigurationReadRepository config)
+		public EventStore (IPersistenceService<IEnumerable<EventStream<AggregateIdentifier>>> persistenceService /*, StreamManagementService streamManager*/, IConfigurationReadRepository config)
 		{
 			eventStreams = new List<EventStream<AggregateIdentifier>>();
 			this.persistenceService = persistenceService;
-		    this.streamManager = streamManager;
+		  //  this.streamManager = streamManager;
 		    this.config = config;
 		}
 
-		//public EventStream<AggregateIdentifier> GetEventStreamForADay (AggregateIdentifier id)
-		//{
-		//	var eventStream = eventStreams.FirstOrDefault(evenstream => evenstream.Id == id);
+		public EventStream<AggregateIdentifier> GetEventStreamForADay (AggregateIdentifier id)
+		{
+			var eventStream = eventStreams.FirstOrDefault(evenstream => evenstream.Id == id);
 
-		//	if (eventStream == null)
-		//	{
-		//		eventStream = new EventStream<AggregateIdentifier>(new AggregateIdentifier(id.Date, 
-		//															                       id.MedicalPracticeId,
-		//															                       config.GetLatestVersionFor(id.MedicalPracticeId)));
-		//		eventStreams.Add(eventStream);
-		//	}
+			if (eventStream == null)
+			{
+				eventStream = new EventStream<AggregateIdentifier>(new AggregateIdentifier(id.Date, 
+																	                       id.MedicalPracticeId,
+																	                       config.GetLatestVersionFor(id.MedicalPracticeId)));
+				eventStreams.Add(eventStream);
+			}
 
-		//	return eventStream;
-		//}
+			return eventStream;
+		}
 
-        public EventStream<AggregateIdentifier> GetEventStreamForADay(AggregateIdentifier id)
-        {
-            var eventStream = eventStreams.FirstOrDefault(evenstream => evenstream.Id == id);
-
-            if (eventStream == null)
-            {
-                eventStream = streamManager.GetEventStream(id);
-            	eventStreams.Add(eventStream);
-            }
-
-            return eventStream;
-        }
+//        public EventStream<AggregateIdentifier> GetEventStreamForADay(AggregateIdentifier id)
+//        {
+//            var eventStream = eventStreams.FirstOrDefault(evenstream => evenstream.Id == id);
+//
+//            if (eventStream == null)
+//            {
+//                eventStream = streamManager.GetEventStream(id);
+//            	eventStreams.Add(eventStream);
+//            }
+//
+//            return eventStream;
+//        }
 
         public void AddEventsToEventStream(AggregateIdentifier id, IEnumerable<DomainEvent> eventStream)
 		{						
@@ -69,14 +68,14 @@ namespace bytePassion.OnkoTePla.Core.Repositories.EventStore
 
 		public void PersistRepository()
 		{
-			//persistenceService.Persist(eventStreams);
-            streamManager.SaveStreams(eventStreams);
+			persistenceService.Persist(eventStreams);
+            //streamManager.SaveStreams(eventStreams);
 		}
 
 		public void LoadRepository()
 		{
-			//eventStreams = persistenceService.Load().ToList();
-		    eventStreams = streamManager.LoadInitialEventStreams();
+			eventStreams = persistenceService.Load().ToList();
+		    //eventStreams = streamManager.LoadInitialEventStreams();
 		}
 	}
 }
