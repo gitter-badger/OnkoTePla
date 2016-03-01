1725893c14e9afb02501e0cc6490bf3be37d4565
Matthias Drescher <kingxelor@live.com>
2016-03-01 10:43:50 +0100

    removed deprecated files

--- bytePassion.OnkoTePla.Client.DataAndService/_deprecated/IDataCenterBuilder.cs
+++
@@ -1,9 +0,0 @@
-//using bytePassion.OnkoTePla.Client.DataAndService.Data;
-//
-//namespace bytePassion.OnkoTePla.Client.DataAndService.Factorys
-//{
-//	public interface IDataCenterBuilder
-//	{
-//		IDataCenter Build();
-//	}
-//}
