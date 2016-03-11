4cc2f70f4e7e8c4eb2edba5079d029aa2dc51968
Matthias Drescher <kingxelor@gmail.com>
2016-03-11 09:39:01 +0100

    server-UI: current seleted Page is optical highlighted

---
+++ bytePassion.OnkoTePla.Server.WpfUi/Computations/ButtonBackgroundColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Media;
+using bytePassion.Lib.WpfLib.ConverterBase;
+using bytePassion.OnkoTePla.Server.WpfUi.Enums;
+
+namespace bytePassion.OnkoTePla.Server.WpfUi.Computations
+{
+	internal class ButtonBackgroundColor : GenericParameterizedValueConverter<MainPage, SolidColorBrush, MainPage>
+	{
+		protected override SolidColorBrush Convert(MainPage value, MainPage parameter, CultureInfo culture)
+		{
+			return value == parameter 
+								? new SolidColorBrush(Colors.Aqua) 
+								: new SolidColorBrush(Colors.White) ;
+		}
+	}
+}
