e46ab7752b20d4d7f518c8bc27fff8ff66ed25bb
Matthias Drescher <kingxelor@gmail.com>
2016-03-18 11:51:49 +0100

    server: data-coherence is sercured

--- bytePassion.OnkoTePla.Contracts/Domain/EventStream.cs
+++ bytePassion.OnkoTePla.Contracts/Domain/EventStream.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using bytePassion.OnkoTePla.Contracts.Domain.Events;
 using bytePassion.OnkoTePla.Contracts.Domain.Events.Base;
 using bytePassion.OnkoTePla.Contracts.Domain.EventStreamUtils;
 
@@ -36,8 +38,57 @@ namespace bytePassion.OnkoTePla.Contracts.Domain
 		}
 
 		private bool IsEventApplicaple(DomainEvent domainEvent)
+		{			
+			var appointmentAddedEvent = domainEvent as AppointmentAdded;
+			if (appointmentAddedEvent != null)
+			{
+				return TryToAddEvent(appointmentAddedEvent);				
+			}
+
+			var appointmentReplacedEvent = domainEvent as AppointmentReplaced;
+			if (appointmentReplacedEvent != null)
+			{
+				return TryToReplaceEvent(appointmentReplacedEvent);				
+			}
+
+			var appointmentDeletedEvent = domainEvent as AppointmentDeleted;
+			if (appointmentDeletedEvent != null)
+			{
+				return TryToDeleteEvent(appointmentDeletedEvent);				
+			}
+
+			throw new ArgumentException();
+		}
+
+		private bool TryToAddEvent(AppointmentAdded addedEvent)
 		{
-			return true;
+			if (eventStreamAggregator.AppointmentData.Any(appointment => appointment.Id == addedEvent.AppointmentId))
+				return false;
+
+			return eventStreamAggregator
+					.AppointmentData
+					.Where(appointment => appointment.TherapyPlaceId == addedEvent.TherapyPlaceId)								 
+					.All(appointment => (appointment.StartTime <= addedEvent.StartTime || appointment.StartTime >= addedEvent.EndTime) &&
+										(appointment.EndTime   <= addedEvent.StartTime || appointment.EndTime   >= addedEvent.EndTime));
+		}
+
+		private bool TryToReplaceEvent(AppointmentReplaced replacedEvent)
+		{
+			if (eventStreamAggregator.AppointmentData.All(appointment => appointment.Id != replacedEvent.OriginalAppointmendId))
+				return false;
+
+			return eventStreamAggregator
+					.AppointmentData
+					.Where(appointment => appointment.TherapyPlaceId == replacedEvent.NewTherapyPlaceId)
+					.Where(appointment => appointment.Id != replacedEvent.OriginalAppointmendId)
+					.All(appointment => (appointment.StartTime <= replacedEvent.NewStartTime || appointment.StartTime >= replacedEvent.NewEndTime) &&
+										(appointment.EndTime   <= replacedEvent.NewStartTime || appointment.EndTime   >= replacedEvent.NewEndTime));
+			
+		}
+
+		private bool TryToDeleteEvent(AppointmentDeleted deletedEvent)
+		{
+			return eventStreamAggregator.AppointmentData.Any(appointment => appointment.Id == deletedEvent.RemovedAppointmentId);
 		}
 	}
 }
