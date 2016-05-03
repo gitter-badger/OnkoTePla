57f215fa6e047a2c910699371e568caa50fc9f5a
Alexander Horn <alexander.horn@astrum-it.de>
2016-05-03 13:30:38 +0200

    added missing BackupInterval.cs and adjusted .gitignore file

---
+++ bytePassion.OnkoTePla.Server.DataAndService/Backup/BackupInterval.cs
@@ -0,0 +1,10 @@
+﻿namespace bytePassion.OnkoTePla.Server.DataAndService.Backup
+    {
+        public enum BackupInterval
+        {
+            Dayly,
+            Weekly,
+            Monthly,
+            None
+        }
+    }
